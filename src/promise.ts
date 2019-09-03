ef02f54bd83a1ce0a7e3b00f1752d33c56d2a948
frankfang <frankfang1990@gmail.com>
2019-09-03 16:07:08 +0800

    revert

--- src/promise.ts
+++ src/promise.ts
@@ -9,23 +9,35 @@ class Promise2 {
       // 遍历 callbacks，调用所有的 handle[0]
       this.callbacks.forEach(handle => {
         if (typeof handle[i] === "function") {
-          let x;
-          try {
-            x = handle[i].call(undefined, data);
-          } catch (e) {
-            return handle[2].reject(e);
-          }
-          handle[2].resolveWith(x);
+          handle[i].call(undefined, data);
         }
       });
     });
   }
 
   resolve(result) {
-    this.resolveOrReject("fulfilled", result, 0);
+    if (this.state !== "pending") return;
+    this.state = "fulfilled";
+    nextTick(() => {
+      // 遍历 callbacks，调用所有的 handle[0]
+      this.callbacks.forEach(handle => {
+        if (typeof handle[0] === "function") {
+          handle[0].call(undefined, result);
+        }
+      });
+    });
   }
   reject(reason) {
-    this.resolveOrReject("rejected", reason, 1);
+    if (this.state !== "pending") return;
+    this.state = "rejected";
+    nextTick(() => {
+      // 遍历 callbacks，调用所有的 handle[0]
+      this.callbacks.forEach(handle => {
+        if (typeof handle[1] === "function") {
+          handle[1].call(undefined, reason);
+        }
+      });
+    });
   }
   constructor(fn) {
     if (typeof fn !== "function") {
@@ -41,65 +53,9 @@ class Promise2 {
     if (typeof fail === "function") {
       handle[1] = fail;
     }
-    handle[2] = new Promise2(() => {});
     this.callbacks.push(handle);
     // 把函数推到 callbacks 里面
-    return handle[2];
-  }
-  resolveWithSelf() {
-    this.reject(new TypeError());
-  }
-  resolveWithPromise(x) {
-    x.then(
-      result => {
-        this.resolve(result);
-      },
-      reason => {
-        this.reject(reason);
-      }
-    );
-  }
-  private getThen(x) {
-    let then;
-    try {
-      then = x.then;
-    } catch (e) {
-      return this.reject(e);
-    }
-    return then;
-  }
-  resolveWithThenable(x) {
-    try {
-      x.then(
-        y => {
-          this.resolveWith(y);
-        },
-        r => {
-          this.reject(r);
-        }
-      );
-    } catch (e) {
-      this.reject(e);
-    }
-  }
-  resolveWithObject(x) {
-    let then = this.getThen(x);
-    if (then instanceof Function) {
-      this.resolveWithThenable(x);
-    } else {
-      this.resolve(x);
-    }
-  }
-  resolveWith(x) {
-    if (this === x) {
-      this.resolveWithSelf();
-    } else if (x instanceof Promise2) {
-      this.resolveWithPromise(x);
-    } else if (x instanceof Object) {
-      this.resolveWithObject(x);
-    } else {
-      this.resolve(x);
-    }
+    return undefined;
   }
 }
 
