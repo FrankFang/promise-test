e19ce7a9fccffb637d7dec54eeb96dfea4ac0c27
frankfang <frankfang1990@gmail.com>
2019-09-03 16:18:16 +0800

    我完善了代码

--- src/promise.ts
+++ src/promise.ts
@@ -2,46 +2,16 @@ class Promise2 {
   state = "pending";
   callbacks = [];
   resolve(result) {
-    if (this.state !== "pending") return;
-    this.state = "fulfilled";
-    nextTick(() => {
-      // 遍历 callbacks，调用所有的 handle[0]
-      this.callbacks.forEach(handle => {
-        if (typeof handle[0] === "function") {
-          handle[0].call(undefined, result);
-        }
-      });
-    });
+    /* 完善 */
   }
   reject(reason) {
-    if (this.state !== "pending") return;
-    this.state = "rejected";
-    nextTick(() => {
-      // 遍历 callbacks，调用所有的 handle[0]
-      this.callbacks.forEach(handle => {
-        if (typeof handle[1] === "function") {
-          handle[1].call(undefined, reason);
-        }
-      });
-    });
+    /* 完善 */
   }
   constructor(fn) {
-    if (typeof fn !== "function") {
-      throw new Error("我只接受函数");
-    }
-    fn(this.resolve.bind(this), this.reject.bind(this));
+    /* 完善 */
   }
   then(succeed?, fail?) {
-    const handle = [];
-    if (typeof succeed === "function") {
-      handle[0] = succeed;
-    }
-    if (typeof fail === "function") {
-      handle[1] = fail;
-    }
-    this.callbacks.push(handle);
-    // 把函数推到 callbacks 里面
-    return undefined;
+    /* 完善 */
   }
 }
 
