b656e7f8103d632e31e9018368707670273b489c
frankfang-test <huayiqishi@qq.com>
2019-09-03 16:35:49 +0800

    Update promise.ts

--- src/promise.ts
+++ src/promise.ts
@@ -2,16 +2,46 @@ class Promise2 {
   state = "pending";
   callbacks = [];
   resolve(result) {
-    console.log('1')
+    if (this.state !== "pending") return;
+    this.state = "fulfilled";
+    nextTick(() => {
+      // 遍历 callbacks，调用所有的 handle[0]
+      this.callbacks.forEach(handle => {
+        if (typeof handle[0] === "function") {
+          handle[0].call(undefined, result);
+        }
+      });
+    });
   }
   reject(reason) {
-    /* 完善 */
+    if (this.state !== "pending") return;
+    this.state = "rejected";
+    nextTick(() => {
+      // 遍历 callbacks，调用所有的 handle[0]
+      this.callbacks.forEach(handle => {
+        if (typeof handle[1] === "function") {
+          handle[1].call(undefined, reason);
+        }
+      });
+    });
   }
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== "function") {
+      throw new Error("我只接受函数");
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this));
   }
   then(succeed?, fail?) {
-    /* 完善 */
+    const handle = [];
+    if (typeof succeed === "function") {
+      handle[0] = succeed;
+    }
+    if (typeof fail === "function") {
+      handle[1] = fail;
+    }
+    this.callbacks.push(handle);
+    // 把函数推到 callbacks 里面
+    return undefined;
   }
 }
 
