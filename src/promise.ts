2d6781be9bca5e161e2b2d08f0ae61c19ad1c495
linshijie <lin_shi_jie@126.com>
2019-09-06 15:37:25 +0800

    完善promise

--- src/promise.ts
+++ src/promise.ts
@@ -3,15 +3,38 @@ class Promise2 {
   callbacks = [];
   resolve(result) {
     /* 完善 */
+    setTimeout(() => {
+      if (this.state !== "pending") return;
+      this.state = "fulfilled";
+      this.callbacks.forEach(handle => {
+        if (typeof handle[0] === "function") {
+          handle[0].call(undefined, result);
+        }
+      });
+    }, 0);
   }
   reject(reason) {
     /* 完善 */
+    setTimeout(() => {
+      if (this.state !== "pending") return;
+      this.state = "rejected";
+      this.callbacks.forEach(handle => {
+        if (typeof handle[1] === "function") {
+          handle[1].call(undefined, reason);
+        }
+      });
+    }, 0);
   }
   constructor(fn) {
     /* 完善 */
+    fn(this.resolve.bind(this), this.reject.bind(this));
   }
   then(succeed?, fail?) {
     /* 完善 */
+    const handle = [];
+    handle[0] = succeed;
+    handle[1] = fail;
+    this.callbacks.push(handle);
   }
 }
 
