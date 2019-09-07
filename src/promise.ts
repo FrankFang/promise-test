7c4ae429d91b347bb0ac83935af623b1dcee3b50
chenyitao <285345240@qq.com>
2019-09-07 23:37:54 +0800

    测试用例通过

--- src/promise.ts
+++ src/promise.ts
@@ -1,17 +1,55 @@
 class Promise2 {
   state = "pending";
-  callbacks = [];
+  STATE_FULFILLED = "fulfilled";
+  STATE_REJECTED = "rejected";
+  STATE_PENDING = "pending";
+  handle: Array<Array<Function>>;
+
+  constructor(fn: Function) {
+    if (this.typeof(fn) !== "function") {
+      throw new Error("构造参数必须是函数");
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this));
+  }
+
   resolve(result) {
-    /* 完善 */
-  }
+    if (this.state !== this.STATE_PENDING) return
+    this.changeState(this.STATE_FULFILLED);
+    nextTick(() => {
+      this.handle.forEach(item => {
+        if (this.typeof(item[0]) === "function") {
+          item[0] && item[0].call(undefined, result);
+        }
+      });
+    });
+  } 
+
   reject(reason) {
-    /* 完善 */
+    if (this.state !== this.STATE_PENDING) return
+    this.changeState(this.STATE_REJECTED);
+    nextTick(() => {
+      this.handle.forEach(item => {
+        if (this.typeof(item[1]) === "function") {
+          item[1].call(undefined, reason);
+        }
+      });
+    });
   }
-  constructor(fn) {
-    /* 完善 */
+
+  private changeState(newState: string) {
+    this.state = this.state === newState ? this.state : newState;
   }
-  then(succeed?, fail?) {
-    /* 完善 */
+
+  then(onFulfilled?: Function, onRejected?: Function) {
+    this.handle = this.handle || [];
+    this.handle.push([onFulfilled, onRejected]);
+  }
+
+  typeof(target: any) {
+    return Object.prototype.toString
+      .call(target)
+      .match(/\[object (.+)\]/)[1]
+      .toLowerCase();
   }
 }
 
