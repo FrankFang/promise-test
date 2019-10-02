803884d9b24b5da12ed66e73fba39ecbb00f4ac4
hy949 <1661346771@qq.com>
2019-10-02 22:42:53 +0800

    完善代码

--- src/promise.ts
+++ src/promise.ts
@@ -1,17 +1,48 @@
 class Promise2 {
   state = "pending";
   callbacks = [];
+
   resolve(result) {
     /* 完善 */
+    setTimeout(() => {
+      if(this.state !== "pending") return
+        this.state = "fulfilled"
+        this.callbacks.forEach(handle => {// 执行成功函数
+          if(typeof handle[0] === "function") {
+            handle[0].call(undefined, result)
+          }
+        })
+    },0)
   }
   reject(reason) {
     /* 完善 */
+    setTimeout(() => {
+      if(this.state === "rejected") return 
+        this.state = "rejected"
+        this.callbacks.forEach(handle => {// 执行失败函数
+          if(typeof handle[1] === "function") {
+            handle[1].call(undefined, reason)
+          }
+      })
+    },0)
   }
   constructor(fn) {
     /* 完善 */
+    if(typeof fn !== 'function') {
+      throw new Error('new Promise的参数应该是一个函数')
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(succeed?, fail?) {
     /* 完善 */
+    let handle = []
+    if(typeof succeed === 'function') {
+      handle[0] = succeed
+    }
+    if(typeof fail === 'function')  {
+      handle[1] = fail
+    }
+      this.callbacks.push(handle)
   }
 }
 
