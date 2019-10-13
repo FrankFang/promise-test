977e4f42d53633261b67d63bc09a351b030afe4d
小木de <1762982273@qq.com>
2019-10-13 19:11:43 +0800

    submit promise.ts

--- src/promise.ts
+++ src/promise.ts
@@ -2,16 +2,40 @@ class Promise2 {
   state = "pending";
   callbacks = [];
   resolve(result) {
-    /* 完善 */
+    if (this.state !== "pending") return
+    this.state = "fulfilled"
+    nextTick(() => {
+      this.callList.forEach(handle => {
+        if (typeof handle[0] === "function") {
+          handle[0].call(undefined, result)
+        }
+      });
+    }, 0)
   }
   reject(reason) {
-    /* 完善 */
+    if (this.state !== "pending") return
+    this.state = "rejected"
+    nextTick(() => {
+      this.callList.forEach(handle => {
+        if (typeof handle[1] === "function") {
+          handle[1].call(undefined, reason)
+        }
+      });
+    }, 0)
   }
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== "function") {
+      throw new Error("只接收函数")
+    }
+    this.state = "pending"
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(succeed?, fail?) {
-    /* 完善 */
+    const handle = []
+    handle[0] = onFulfilled
+    handle[1] = onRejected
+    this.callList.push(handle)
+    return undefined
   }
 }
 
