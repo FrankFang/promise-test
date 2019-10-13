7366de76b82883c47b56e2fed604d3f26a225edc
小木de <1762982273@qq.com>
2019-10-13 19:15:43 +0800

    fix callbacks

--- src/promise.ts
+++ src/promise.ts
@@ -5,7 +5,7 @@ class Promise2 {
     if (this.state !== "pending") return
     this.state = "fulfilled"
     nextTick(() => {
-      this.callList.forEach(handle => {
+      this.callbacks.forEach(handle => {
         if (typeof handle[0] === "function") {
           handle[0].call(undefined, result)
         }
@@ -16,7 +16,7 @@ class Promise2 {
     if (this.state !== "pending") return
     this.state = "rejected"
     nextTick(() => {
-      this.callList.forEach(handle => {
+      this.callbacks.forEach(handle => {
         if (typeof handle[1] === "function") {
           handle[1].call(undefined, reason)
         }
@@ -34,7 +34,7 @@ class Promise2 {
     const handle = []
     handle[0] = onFulfilled
     handle[1] = onRejected
-    this.callList.push(handle)
+    this.callbacks.push(handle)
     return undefined
   }
 }
