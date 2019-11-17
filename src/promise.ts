7f47342b96019cfb17d5708f885768f409c1f6a7
王柏 <814463173@qq.com>
2019-11-17 20:38:37 +0800

    模拟promise（then）

--- src/promise.ts
+++ src/promise.ts
@@ -2,16 +2,43 @@ class Promise2 {
   state = "pending";
   callbacks = [];
   resolve(result) {
-    /* 完善 */
+    setTimeout(() => {
+      if(this.state === 'pending') return
+      this.state = 'fulfilled'
+      this.callbacks.forEach((handle) => {
+          if(typeof handle[0] === 'function') {
+              handle[0].call(undefined, result)
+          }
+      })
+    })
   }
   reject(reason) {
-    /* 完善 */
+    setTimeout(() => {
+        if(this.state === 'pending') return
+        this.state = 'rejected'
+        this.callbacks.forEach((handle) => {
+            if(typeof handle[1] === 'function') {
+              handle[1].call(undefined, reason)
+            }
+        })
+    })
   }
   constructor(fn) {
-    /* 完善 */
+    if(typeof fn !== 'function') {
+      throw new Error('只接收函数！')
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(succeed?, fail?) {
-    /* 完善 */
+    let handle = []
+    if(typeof succeed === 'function') {
+        handle[0] = succeed
+    }
+    if(typeof fail === 'function') {
+        handle[1] = fail
+    }
+    this.callbacks.push(handle)
+    return this
   }
 }
 
