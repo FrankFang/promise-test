6d6f6249cfdde9315e1897b976fc620c876b2381
zch <529743595@qq.com>
2019-12-08 19:58:33 +0800

    Update promise.ts

--- src/promise.ts
+++ src/promise.ts
@@ -1,18 +1,44 @@
-class Promise2 {
-  state = "pending";
-  callbacks = [];
-  resolve(result) {
-    /* 完善 */
+
+const isFunction = function (fn:any) {
+  if (typeof fn !== 'function') return false
+  return true
+}
+
+export default function Promise2(fn: Function) {
+  if (!isFunction(fn)) throw Error('Promise resolver undefined is not a function')
+  this.events = []
+  this.status = 'pending'
+  fn.call(undefined, this.resolve.bind(this), this.reject.bind(this))
+}
+
+myPromise.prototype.then = function (success?: Function, fail?: Function) {
+  if (isFunction(success) || isFunction(fail)) {
+    this.events.push([success, fail])
   }
-  reject(reason) {
-    /* 完善 */
-  }
-  constructor(fn) {
-    /* 完善 */
-  }
-  then(succeed?, fail?) {
-    /* 完善 */
-  }
+}
+
+myPromise.prototype.resolve = function (value: any) {
+  setTimeout(() => {
+    if (this.status !== 'pending') return
+    this.status = 'fulfilled'
+    this.events.map((fn: [Function, Function]) => {
+      if (isFunction(fn[0])) {
+        fn[0].call(undefined, value)
+      }
+    })
+  })
+}
+
+myPromise.prototype.reject = function (reason: any) {
+  setTimeout(() => {
+    if (this.status !== 'pending') return
+    this.status = 'rejected'
+    this.events.map((fn: [Function, Function]) => {
+      if (isFunction(fn[1])) {
+        fn[1].call(undefined, reason)
+      }
+    })
+  })
 }
 
 export default Promise2;
