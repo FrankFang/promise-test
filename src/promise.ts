540fbcce159c9d759651fc4780b8993a1d8e9e52
Honohonoho <751718620@qq.com>
2020-01-11 14:21:04 +0800

    update promise

--- src/promise.ts
+++ src/promise.ts
@@ -3,15 +3,45 @@ class Promise2 {
   callbacks = [];
   resolve(result) {
     /* 完善 */
+    if (this.state !== 'pending') return
+    this.state = 'fulfilled'
+    setTimeout(() => {
+      this.callbacks.forEach(handle => {
+        if (typeof handle[0] === 'function') {
+          handle[0].call(undefined, result)
+        }
+      })
+    }, 0)
   }
   reject(reason) {
     /* 完善 */
+    if (this.state !== 'pending') return
+    this.state = 'rejected'
+    setTimeout(() => {
+      this.callbacks.forEach(handle => {
+        if (typeof handle[1] === 'function') {
+          handle[1].call(undefined, reason)
+        }
+      })
+    }, 0)
   }
   constructor(fn) {
     /* 完善 */
+    if (typeof fn !== 'function') {
+      throw new Error('new Promise should accept a function')
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(succeed?, fail?) {
     /* 完善 */
+    let handle = []
+    if (typeof succeed === 'function') {
+      handle[0] = succeed
+    }
+    if (typeof fail === 'function') {
+      handle[1] = fail
+    }
+    this.callbacks.push(handle)
   }
 }
 
