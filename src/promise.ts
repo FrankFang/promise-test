715ce209a2c05b39be258267ff8984f7fdc78f3d
gmYuan <ygm121351@163.com>
2020-01-21 15:22:13 +0800

    完善代码

--- src/promise.ts
+++ src/promise.ts
@@ -1,21 +1,65 @@
 class Promise2 {
   state = "pending";
   callbacks = [];
+
   resolve(result) {
-    /* 完善 */
+    if (this.state !== 'pending') return;
+    this.state = 'fulfilled'
+    setTimeout( ()=> {
+      this.callbacks.forEach(handle => {
+        if (typeof handle[0] === 'function') {
+          handle[0].call(undefined, result)
+        }
+      })
+    }, 0)
+
   }
+
+
   reject(reason) {
-    /* 完善 */
+    if (this.state !== 'pending') return;
+    this.state = 'rejected'
+
+    setTimeout( ()=> {
+      this.callbacks.forEach(handle => {
+        if (typeof handle[1] === 'function') {
+          handle[1].call(undefined, reason)
+        }
+      })
+
+    }, 0)
+
   }
+
+  
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== 'function') {
+      throw new Error("Promise接收的参数是 函数")
+    }
+
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
+
+
   then(succeed?, fail?) {
-    /* 完善 */
+    let handler = []
+    if (typeof succeed === 'function') {
+      handler[0] = succeed
+    }
+
+    if (typeof fail === 'function') {
+      handler[1] = fail
+    }
+
+    this.callbacks.push(handler)
+
   }
 }
 
 export default Promise2;
+
+
+
 
 function nextTick(fn) {
   if (process !== undefined && typeof process.nextTick === "function") {
