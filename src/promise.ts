6405aa3d0f6ef4812d4d3e84c216f6a2dd519e71
a-tione <heyanggo@gmail.com>
2020-02-23 17:31:13 +0800

    完善promise代码

--- src/promise.ts
+++ src/promise.ts
@@ -2,16 +2,41 @@ class Promise2 {
   state = "pending";
   callbacks = [];
   resolve(result) {
-    /* 完善 */
+    if (this.state !== 'pending') return
+    this.state = 'fulfilled'
+    setTimeout(()=>{
+      this.callbacks.forEach(handle=>{
+        if (typeof handle[0] === "function") {
+          handle[0].call(undefined, result)
+        }
+      })
+    }, 0)
   }
   reject(reason) {
-    /* 完善 */
+    if (this.state !== 'pending') return
+    this.state = 'rejected'
+    setTimeout(()=>{
+      this.callbacks.forEach(handle=>{
+      if (typeof handle[1] === "function") {
+        handle[1].call(undefined, reason)
+      }
+    })
+    }, 0)
   }
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== "function") return
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(succeed?, fail?) {
-    /* 完善 */
+    const handle = []
+    if (typeof succeed === "function") {
+      handle[0] = succeed
+    }
+    if (typeof succeed === "function") {
+      handle[1] = fail
+    }
+    this.callbacks.push(handle)
+    return undefined
   }
 }
 
