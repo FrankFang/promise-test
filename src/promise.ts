31f0ec83753e0b3b10db7569df80e755a0537b42
yym-yumeng123 <18026493873@163.com>
2020-05-10 17:21:12 +0800

    完善promise

--- src/promise.ts
+++ src/promise.ts
@@ -1,17 +1,57 @@
 class Promise2 {
-  state = "pending";
-  callbacks = [];
+  /**
+   * @params {promise then 可以多次调用}
+   * [[succees, fail], [success, fail]]
+   * */ 
+  callbacks = []
+
+  // 状态
+  state = "pending"
   resolve(result) {
-    /* 完善 */
+    setTimeout(() => {
+      if (this.state !== "pending") return
+      this.state = "fulfilled"
+
+      this.callbacks.forEach(handler => {
+        if (typeof handler[0] === "function") {
+          handler[0].call(undefined, result)
+        }
+      })
+      
+    }, 0)
   }
   reject(reason) {
-    /* 完善 */
+    setTimeout(() => {
+      if (this.state !== "pending") return
+      this.state = "rejected"
+
+      this.callbacks.forEach(handler => {
+        if (typeof handler[1] === "function") {
+          handler[1].call(undefined, reason)
+        }
+      })
+      
+    }, 0)
   }
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== "function") {
+      throw new Error("我只接收函数")
+    }
+
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
+
   then(succeed?, fail?) {
-    /* 完善 */
+    const handler = []
+    if (typeof succeed === "function") {
+      handler[0] = succeed
+    }
+    if (typeof fail === "function") {
+      handler[1] = fail
+    }
+
+    // then 可以多次调用
+    this.callbacks.push(handler)
   }
 }
 
