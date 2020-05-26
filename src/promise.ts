5fb8a4212b4a796a77fe4a1d8e7a35dd91b179a1
TdArita <tang601619508@gmail.com>
2020-05-26 23:55:49 +0800

    完善promise

--- src/promise.ts
+++ src/promise.ts
@@ -1,17 +1,47 @@
 class Promise2 {
   state = "pending";
   callbacks = [];
-  resolve(result) {
-    /* 完善 */
+resolve(result) {
+    setTimeout(() => {
+      //遍历callbacks 调用所有callbacks[0]
+      if (this.state !== 'pending') return
+      this.state = "fulfilled"
+      this.callbacks.forEach((handle) => {
+        if (typeof handle[0] === 'function') {
+          handle[0].call(undefined, result)
+        }
+      })
+    }, 0);
   }
   reject(reason) {
-    /* 完善 */
+    setTimeout(() => {
+      //遍历callbacks 调用所有callbacks[1]
+      if (this.state !== 'pending') return
+      this.state = "rejected"
+      this.callbacks.forEach((handle) => {
+        if (typeof handle[1] === 'function') {
+          handle[1].call(undefined, reason)
+        }
+      })
+    }, 0);
   }
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== 'function') {
+      throw new Error("我只接受函数")
+    }
+
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
+
   then(succeed?, fail?) {
-    /* 完善 */
+    const handle = []
+    if (typeof succeed === 'function') {
+      handle[0] = succeed
+    }
+    if (typeof fail === 'function') {
+      handle[1] = fail
+    }
+    this.callbacks.push(handle) //把函数推到callbacks里
   }
 }
 
