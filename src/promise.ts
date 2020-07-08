238ead3adf710fc32d2f5c5a671b28c9cd2bd713
CFCode-git <435184766@qq.com>
2020-07-08 17:25:56 +0800

    finish

--- src/promise.ts
+++ src/promise.ts
@@ -1,24 +1,54 @@
 class Promise2 {
-  state = "pending";
+  state = 'pending';
   callbacks = [];
+
   resolve(result) {
-    /* 完善 */
+    if (this.state !== 'pending') return;
+    this.state = 'fulfilled';
+    setTimeout(() => {
+      this.callbacks.forEach(handle => {
+        if (typeof handle[0] === 'function') {
+          handle[0].call(undefined, result);
+        }
+      });
+    }, 0);
   }
+
   reject(reason) {
-    /* 完善 */
+    if (this.state !== 'pending') return;
+    this.state = 'rejected';
+    setTimeout(() => {
+      this.callbacks.forEach(handle => {
+        if (typeof handle[1] === 'function') {
+          handle[1](reason);
+        }
+      });
+    }, 0);
   }
+
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== 'function') {
+      throw new Error('劳资只接收函数');
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this));
   }
+
   then(succeed?, fail?) {
-    /* 完善 */
+    const handle = [];
+    if (typeof succeed === 'function') {
+      handle[0] = succeed;
+    }
+    if (typeof fail === 'function') {
+      handle[1] = fail;
+    }
+    this.callbacks.push(handle);
   }
 }
 
 export default Promise2;
 
 function nextTick(fn) {
-  if (process !== undefined && typeof process.nextTick === "function") {
+  if (process !== undefined && typeof process.nextTick === 'function') {
     return process.nextTick(fn);
   } else {
     var counter = 1;
