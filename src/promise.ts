7a39817e6733aba838e02f951b8074443e268b58
邓国新 <1414894071@qq.com>
2020-12-06 17:41:46 +0800

    我完善了

--- src/promise.ts
+++ src/promise.ts
@@ -1,20 +1,49 @@
 class Promise2 {
-  state = "pending";
-  callbacks = [];
-  resolve(result) {
-    /* 完善 */
+  callbacks = []
+  state = "pending"
+  resolve(result){
+    setTimeout(()=>{
+      if(this.state !== "pending"){return}
+      this.state = "fulfilled"
+      this.callbacks.forEach((handle)=>{
+        if(typeof handle[0] === 'function'){
+          handle[0].call(undefined,result)
+        }
+      })
+
+
+    },0)
   }
-  reject(reason) {
-    /* 完善 */
+  reject(reason){
+    if(this.state !== "pending"){return}
+    this.state = "rejected"
+    setTimeout(()=>{
+      this.callbacks.forEach((handle)=>{
+        if(typeof handle[1] === "function"){
+          handle[1].call(undefined,reason)
+        }
+      })
+
+
+    },0)
   }
   constructor(fn) {
-    /* 完善 */
+    if(typeof fn !== "function"){
+      throw new Error("我只接受函数")
+    }
+    fn(this.resolve.bind(this),this.reject.bind(this))
   }
-  then(succeed?, fail?) {
-    /* 完善 */
+  then(success?,fail?){
+    const handle = []
+    if(typeof success === "function"){
+      handle[0] = success
+    }
+    if(typeof fail === "function"){
+      handle[1] = fail
+    }
+    this.callbacks.push(handle)
   }
 }
-
 export default Promise2;
 
 function nextTick(fn) {
