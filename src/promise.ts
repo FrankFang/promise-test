7824d2462d3f300629192270857b54b0128d981e
liu zhenbin <536543269@qq.com>
2021-01-15 09:34:29 +0800

    [feat] 完善Promise

--- src/promise.ts
+++ src/promise.ts
@@ -3,15 +3,42 @@ class Promise2 {
   callbacks = [];
   resolve(result) {
     /* 完善 */
+    if(this.state!=='pending') return
+    this.state = 'fulfilled'
+    nextTick(() => {
+      this.callbacks.forEach(handle=>{
+        handle[0] && handle[0].call(undefined, result)
+      })
+    });
   }
   reject(reason) {
     /* 完善 */
+    if(this.state!=='pending') return
+    this.state = 'rejected'
+    nextTick(() => {
+      this.callbacks.forEach(handle=>{
+        handle[1] && handle[1].call(undefined,reason)
+      })
+    });
   }
   constructor(fn) {
     /* 完善 */
+    if(typeof fn!=='function'){
+      throw new Error('fn 必须是一个函数')
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(succeed?, fail?) {
     /* 完善 */
+    const handle = []
+    if(typeof succeed==='function'){
+      handle[0] = succeed
+    }
+    if(typeof fail ==='function'){
+      handle[1] = fail
+    }
+    this.callbacks.push(handle)
+    return undefined
   }
 }
 
