e97015da819c5e0a7a7cae99e3038eecdae969a9
sara.wang <34567099+Saraalwayslikeworld@users.noreply.github.com>
2021-04-17 16:04:19 +0800

    完成promise

--- src/promise.ts
+++ src/promise.ts
@@ -1,17 +1,93 @@
 class Promise2 {
-  state = "pending";
+    state = "pending";
   callbacks = [];
-  resolve(result) {
-    /* 完善 */
-  }
-  reject(reason) {
-    /* 完善 */
-  }
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== "function") {
+      throw new Error("只接受函数作为参数");
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this));
   }
-  then(succeed?, fail?) {
-    /* 完善 */
+  private resolveOrReject(state, data, i) {
+    if (this.state !== "pending") return;
+    this.state = state;
+    nextTick(() => {
+      this.callbacks.forEach((handle) => {
+        if (typeof handle[i] === "function") {
+          try {
+            let x = handle[i].call(undefined, data);
+            handle[2].resolveWith(x);
+          } catch (error) {
+            handle[2].reject(error);
+          }
+        }
+      });
+    });
+  }
+  resolve(result?: unknown) {
+    this.resolveOrReject("fulfilled", result, 0);
+  }
+  reject(reason?: unknown) {
+    this.resolveOrReject("rejected", reason, 1);
+  }
+  then(onFulfilled?, onRejected?) {
+    const handle = [];
+    if (typeof onFulfilled === "function") {
+      handle[0] = onFulfilled;
+    }
+    if (typeof onRejected === "function") {
+      handle[1] = onRejected;
+    }
+    handle[2] = new MyPromise(() => {});
+    this.callbacks.push(handle);
+    return handle[2];
+  }
+  resolveWith(x) {
+    if (this === x) {
+      this.resolveWithSelf();
+    } else if (x instanceof MyPromise) {
+      this.resolveWithPromise(x);
+    } else if (x instanceof Object) {
+      this.resolveWithObject(x);
+    } else {
+      this.resolve(x);
+    }
+  }
+  resolveWithSelf() {
+    throw new TypeError();
+  }
+  resolveWithPromise(x) {
+    x.then(
+      (result) => {
+        this.resolve(result);
+      },
+      (reason) => {
+        this.reject(reason);
+      }
+    );
+  }
+  resolveWithObject(x) {
+    let then;
+    try {
+      then = x.then;
+    } catch (e) {
+      throw new Error(e);
+    }
+    if (then instanceof Function) {
+      try {
+        x.then(
+          (result) => {
+            this.resolveWith(result);
+          },
+          (reason) => {
+            this.reject(reason);
+          }
+        );
+      } catch (error) {
+        this.reject(error);
+      }
+    } else {
+      this.resolve(x);
+    }
   }
 }
 
