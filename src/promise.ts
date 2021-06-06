de403090e0a467c0c32cc82a69337898ed84d102
zhouyijun <zhou495248579@hotmail.com>
2021-06-06 18:09:16 +0800

    complete

--- src/promise.ts
+++ src/promise.ts
@@ -1,18 +1,57 @@
-class Promise2 {
-  state = "pending";
-  callbacks = [];
-  resolve(result) {
-    /* 完善 */
+ function isFunction(value: any) {
+  return typeof value === "function";
+}
+
+export enum State {
+  pending,
+  fulfilled,
+  rejected,
+}
+type onFulfilled = (data: unknown) => void;
+type onRejected = (error: unknown) => void;
+type handle = [onFulfilled | undefined, onRejected | undefined];
+export class Promise2 {
+  callBack: handle[] = [];
+  state = State.pending;
+  resolve(data: unknown) {
+    if (this.state !== State.pending) {
+      return;
+    }
+    this.state = State.fulfilled;
+    setTimeout(() => {
+      this.callBack.forEach((item) => {
+        item[0]?.call(undefined, data);
+      });
+    });
   }
-  reject(reason) {
-    /* 完善 */
+  reject(error: unknown) {
+    if (this.state !== State.pending) {
+      return;
+    }
+    this.state = State.rejected;
+    setTimeout(() => {
+      this.callBack.forEach((item) => {
+        item[1]?.call(undefined, error);
+      });
+    });
   }
-  constructor(fn) {
-    /* 完善 */
+  constructor(fn: (res: unknown, rej: unknown) => void) {
+    if (typeof fn !== "function") {
+      throw new Error("fn 不是一个函数");
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this));
   }
-  then(succeed?, fail?) {
-    /* 完善 */
-  }
+
+  then = (onFulfilled?: onFulfilled, onRejected?: onRejected) => {
+    const handle: handle = [undefined, undefined];
+    if (isFunction(onFulfilled)) {
+      handle[0] = onFulfilled;
+    }
+    if (isFunction(onRejected)) {
+      handle[1] = onRejected;
+    }
+    this.callBack.push(handle);
+  };
 }
 
 export default Promise2;
