68387abb7db13dbf82f02f2a583453cfbae1d69f
bela <1981253197@qq.com>
2021-10-04 21:54:14 +0800

    完善promise代码

--- src/promise.ts
+++ src/promise.ts
@@ -3,15 +3,47 @@ class Promise2 {
   callbacks = [];
   resolve(result) {
     /* 完善 */
+    if (this.state !== "pending") {
+      return
+    }
+    this.state = 'fulfilled'
+    nextTick(() => {
+      this.callbacks.forEach(fns => {
+        typeof fns[0] === 'function' && fns[0].call(undefined, result)
+      })
+    })
   }
   reject(reason) {
     /* 完善 */
+    if (this.state !== "pending") {
+      return
+    }
+    this.state = 'rejected'
+    nextTick(() => {
+      this.callbacks.forEach(fns => {
+        typeof fns[1] === 'function' && fns[1].call(undefined, reason)
+      })
+    })
+
   }
   constructor(fn) {
     /* 完善 */
+    if (typeof fn !== 'function') {
+      throw new Error("必须是一个函数")
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
+
   then(succeed?, fail?) {
     /* 完善 */
+    const tempArr = []
+    if (typeof succeed === 'function') {
+      tempArr[0] = succeed
+    }
+    if (typeof fail === 'function') {
+      tempArr[1] = fail
+    }
+    this.callbacks.push(tempArr)
   }
 }
 
