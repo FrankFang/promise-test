101aaa0e295d19dbb2a5da304edd294c79ba3634
Llane00 <bluetinestyle@126.com>
2021-11-16 12:37:48 +0800

    feat: 完善Promise类的代码通过测试

--- src/promise.ts
+++ src/promise.ts
@@ -1,35 +1,63 @@
 class Promise2 {
-  state = "pending";
-  callbacks = [];
+  state = 'pending'
+  callbacks = []
+
   resolve(result) {
-    /* 完善 */
+    setTimeout(() => {
+      if (this.state !== 'pending') return
+      this.state = 'fulfilled'
+      for (let handle of this.callbacks) {
+        if (typeof handle[0] === 'function') {
+          handle[0].call(undefined, result)
+        }
+      }
+    }, 0)
   }
   reject(reason) {
-    /* 完善 */
+    setTimeout(() => {
+      if (this.state !== 'pending') return
+      this.state = 'rejected'
+      for (let handle of this.callbacks) {
+        if (typeof handle[1] === 'function') {
+          handle[1].call(undefined, reason)
+        }
+      }
+    }, 0)
   }
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== 'function') {
+      throw new Error('Promise的参数需要是一个函数')
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
   then(succeed?, fail?) {
-    /* 完善 */
+    let handle = []
+    if (typeof succeed === 'function') {
+      handle[0] = succeed
+    }
+
+    if (typeof fail === 'function') {
+      handle[1] = fail
+    }
+    this.callbacks.push(handle)
   }
 }
 
-export default Promise2;
+export default Promise2
 
 function nextTick(fn) {
-  if (process !== undefined && typeof process.nextTick === "function") {
-    return process.nextTick(fn);
+  if (process !== undefined && typeof process.nextTick === 'function') {
+    return process.nextTick(fn)
   } else {
-    var counter = 1;
-    var observer = new MutationObserver(fn);
-    var textNode = document.createTextNode(String(counter));
+    var counter = 1
+    var observer = new MutationObserver(fn)
+    var textNode = document.createTextNode(String(counter))
 
     observer.observe(textNode, {
-      characterData: true
-    });
+      characterData: true,
+    })
 
-    counter = counter + 1;
-    textNode.data = String(counter);
+    counter = counter + 1
+    textNode.data = String(counter)
   }
 }
