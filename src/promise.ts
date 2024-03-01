61ec663c4ef7311bbbf0af876cd568b2d4b0168d
xlsama <im.xlsama@gmail.com>
2024-03-01 11:03:28 +0800

    chore: update

--- src/promise.ts
+++ src/promise.ts
@@ -1,35 +1,44 @@
 class Promise2 {
-  state = "pending";
-  callbacks = [];
-  resolve(result) {
-    /* 完善 */
-  }
-  reject(reason) {
-    /* 完善 */
-  }
+  state = 'pending'
+  callbacks = []
+
   constructor(fn) {
-    /* 完善 */
+    if (typeof fn !== 'function') {
+      throw new TypeError('not a function')
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
   }
+
+  resolve(result) {
+    setTimeout(() => {
+      if (this.state !== 'pending') return
+      this.callbacks.forEach(handle => {
+        if (typeof handle[0] === 'function') {
+          handle[0].call(undefined, result)
+          this.state = 'fulfilled'
+        }
+      })
+    }, 0)
+  }
+
+  reject(reason) {
+    setTimeout(() => {
+      if (this.state !== 'pending') return
+      this.callbacks.forEach(handle => {
+        if (typeof handle[1] === 'function') {
+          handle[1].call(undefined, reason)
+          this.state = 'rejected'
+        }
+      })
+    }, 0)
+  }
+
   then(succeed?, fail?) {
-    /* 完善 */
+    const handle = []
+    handle[0] = succeed
+    handle[1] = fail
+    this.callbacks.push(handle)
   }
 }
 
-export default Promise2;
-
-function nextTick(fn) {
-  if (process !== undefined && typeof process.nextTick === "function") {
-    return process.nextTick(fn);
-  } else {
-    var counter = 1;
-    var observer = new MutationObserver(fn);
-    var textNode = document.createTextNode(String(counter));
-
-    observer.observe(textNode, {
-      characterData: true
-    });
-
-    counter = counter + 1;
-    textNode.data = String(counter);
-  }
-}
+export default Promise2
